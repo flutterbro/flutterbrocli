d53ffa419034565816a4ebc1dae7161ec8ec082a
flutterbro <flutterbro@yandex.ru>
2021-03-21 09:32:58 +0300

    Clear main.dart.

--- lib/src/flutterbrocli.dart
+++ lib/src/flutterbrocli.dart
@@ -1,13 +1,16 @@
 import 'dart:io';
 
 const _pubSpecYaml = 'pubspec.yaml';
+const _mainDart = 'lib/main.dart';
 const _newLine = '\n';
 final _yamlCommentsPattern = RegExp(r'.*#.*');
+final _dartCommentsPattern = RegExp(r''); // TODO
 
 class SetupUtils {
-  static Future<void> clearPubSpecYaml() async {
+  static Future<bool> clearPubSpecYaml() async {
     final file = File(_pubSpecYaml);
-    if (await file.exists()) {
+    final exists = await file.exists();
+    if (exists) {
       final lines = await file.readAsLines();
       final raw = lines
           .where((element) => !element.contains(_yamlCommentsPattern))
@@ -19,6 +22,25 @@ class SetupUtils {
       await file.create();
       await file.writeAsString(cleared);
     }
+    return exists;
+  }
+
+  static Future<void> clearMainDart() async {
+    final file = File(_mainDart);
+    final exists = await file.exists();
+    if (exists) {
+      final lines = await file.readAsLines();
+      final raw = lines
+          .where((element) => !element.contains(_dartCommentsPattern))
+          .join(_newLine);
+      final cleared = raw
+          .replaceAll(_dartCommentsPattern, '')
+          .replaceAll(_newLine + _newLine + _newLine, _newLine + _newLine);
+      await file.delete();
+      await file.create();
+      await file.writeAsString(cleared);
+    }
+    return exists;
   }
 
   const SetupUtils._();
